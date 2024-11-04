61dc2dfbc98367a00847b034271254cbfc5f8c8c
Amatterazzu <121918534+Amatterazzu@users.noreply.github.com>
2024-11-03 21:07:53 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ import openpyxl
 from openpyxl.styles import Alignment, Font
 import os
 
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'edu7701')
 
